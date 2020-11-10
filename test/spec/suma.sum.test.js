888b3ed4ccfafd03214d842ad7d26c9fecfe3560
RaulBenitezM <benitezmillet.raul2@gmail.com>
2020-11-10 19:19:07 +0100

    Cambios respecto a las prácticas anteriores

--- test/spec/suma.sum.test.js
+++ test/spec/suma.sum.test.js
@@ -1,4 +1,4 @@
-const p1Tests = require('../../app/scripts/p1.tests.js');
+const p1Tests = require('../../app/scripts/suma.tests.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(p1Tests.sum(1, 2)).toBe(3);
