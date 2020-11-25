c3e2b0e411ce99281c29b5b86c3bef4dd518c9e6
RaulBenitezM <benitezmillet.raul2@gmail.com>
2020-11-25 21:43:33 +0100

    Correcion Practica 2 BOM/DOM/Cookies

--- app/scripts/practicas_bom/p2.js
+++ app/scripts/practicas_bom/p2.js
@@ -11,27 +11,48 @@ function expiracionCookie(dias) {
   return expires;
 }
 
-//Separo la cookie en nombre y valor
-let [nombre, valor] = document.cookie.split('=');
+//Separo las cookies para crear un array de todas
+let cookies = document.cookie.split(';');
+let nombreVisitas = '';
+let valorVisitas;
+
+//Busco la cookie llamada "visitas" y la guardo
+for (cookie of cookies) {
+  let [nombre, valor] = cookie.split('=');
+
+  if (nombre == 'visitas') {
+    nombreVisitas = nombre;
+    valorVisitas = valor;
+  }
+}
 
 //Si la cookie no existe, se crea con valor 1 y expiración en un mes
 //En cambio, si existe pero el valor es superior a 9, el valor se reestablece a 0 y se borra la cookie
 //Por último, si no pasa nada de lo anterior, el valor de la cookie se incrementa en 1
-if (nombre === '') {
-  valor = 1;
+if (nombreVisitas === '') {
+  valorVisitas = 1;
+
   document.cookie =
-    'visitas=' + valor + ';expires=' + expiracionCookie(30).toUTCString();
-} else if (valor > 9) {
-  valor = 0;
+    'visitas=' +
+    valorVisitas +
+    ';expires=' +
+    expiracionCookie(30).toUTCString();
+} else if (valorVisitas > 9) {
+  valorVisitas = 0;
+
   document.cookie =
-    'visitas=' + valor + ';expires=Sat, 01 Jan 2000 00:00:01 GMT';
+    'visitas=' + valorVisitas + ';expires=Sat, 01 Jan 2000 00:00:01 GMT';
 } else {
-  ++valor;
+  ++valorVisitas;
+
   document.cookie =
-    'visitas=' + valor + ';expires=' + expiracionCookie(30).toUTCString();
+    'visitas=' +
+    valorVisitas +
+    ';expires=' +
+    expiracionCookie(30).toUTCString();
 }
 
-let textoContador = document.createTextNode(valor);
+let textoContador = document.createTextNode(valorVisitas);
 
 //Muestro el valor de la cookie en el documento HTML
 document.getElementById('contador').appendChild(textoContador);
